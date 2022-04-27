d16c6415bdd5e9061be336d765efd77b9675b32c
Sarang Kulkarni <kulkarnisarang01@gmail.com>
2022-04-27 17:16:23 +0530

    Assignment: More About Class

---
+++ block-BJaahy/code/script.js
@@ -0,0 +1,17 @@
+class Square{
+    constructor(side){
+        this.width = side;
+        this.height = side;
+    }
+    description(){
+        alert(`The square is ${width} x ${height}`);
+    }
+    calcArea(){
+        let area = width * height;
+        return area,
+    }
+    get area(value){
+
+    }
+
+}
