75016ba86ed9c1f907eadc76865acf730db2aeec
Sarang Kulkarni <kulkarnisarang01@gmail.com>
2022-04-30 15:37:16 +0530

    Assignment: Stack and Queue

---
+++ block-BJaaii/code/script.js
@@ -0,0 +1,74 @@
+class Stack {
+    constructor(stack){
+        this.stack = stack;
+    }
+    push(value){
+        for(let i = 0; i < this.length; i++){
+            this.push(value);
+        }
+    }
+    pop(){
+        for(let i = this.length; i <0 ; i--){
+            this.pop(i);
+        }
+    }
+    peek(){
+        return this.length;
+    }
+    reverse(){
+        for(let i = 0; i < this.length; i++){
+            this.reverse();
+        }
+    }
+    isEmpty(){
+        for(let i = 0; i < this.length; i++){
+           if(this.stack === 0){
+               return true;
+           } else{
+               return false;
+           }
+        }
+    }
+    displayStack(){
+        return `${this.stack}`;
+    }
+    get length(){
+        return this.stack;
+    }
+}
+
+
+// class Queue
+class Queue {
+    constructor(queue){
+        this.queue = queue;
+    }
+    enqueue(item){
+        for(let i = 0; i < this.length; i++){
+            this.push(item);
+        }
+    }
+    dequeue(){
+        for(let i = this.length; i <0 ; i--){
+            this.pop(i);
+        }
+    }
+    peek(){
+        return this.length;
+    }
+    isEmpty(){
+        for(let i = 0; i < this.length; i++){
+           if(this.queue === 0){
+               return true;
+           } else{
+               return false;
+           }
+        }
+    }
+    displayQueue(){
+        return `${this.queue}`;
+    }
+    get length(){
+        return this.queue;
+    }
+}
